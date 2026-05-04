3f21135c1eb5a9b0784e4566ffd7f59760251605
EEEmryc <emrychumeau@gmail.com>
2026-05-04 15:20:43 +0200

    tests

--- src/test/java/com/example/demo/data/VoitureTest.java
+++ src/test/java/com/example/demo/data/VoitureTest.java
@@ -7,19 +7,9 @@ class VoitureTest {
 
     @Test
     void testCreerVoiture() {
-        // Arrange : On prépare l'objet
-        Voiture voiture = new Voiture("Toyota", "Bleu");
+        Voiture voiture = new Voiture("Toyota", 100);
 
-        // Act & Assert : On vérifie que les données sont bien enregistrées
         assertNotNull(voiture, "L'objet voiture ne doit pas être nul");
         assertEquals("Toyota", voiture.getMarque(), "La marque doit être Toyota");
-        assertEquals("Bleu", voiture.getCouleur(), "La couleur doit être Bleu");
     }
 
-    @Test
-    void testChangementCouleur() {
-        Voiture voiture = new Voiture("Toyota", "Bleu");
-        voiture.setCouleur("Rouge");
-        assertEquals("Rouge", voiture.getCouleur(), "La couleur doit être passée au rouge");
-    }
-}
