07e4cb90d0aa9e737871ee497cc389cd6609ada0
HUMEAU Emryc <12202356@t20621.iutv.univ-paris13.fr>
2026-05-11 10:11:13 +0200

    Changement de StatistiqueTests avec Mockito

--- src/test/java/com/example/demo/service/StatistiqueTests.java
+++ src/test/java/com/example/demo/service/StatistiqueTests.java
@@ -1,17 +1,37 @@
 package com.example.demo.service;
 
-import com.example.demo.data.Voiture;
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
-
+import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
 
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import java.util.List;
+import com.example.demo.data.Voiture;
 
-@SpringBootTest
-public class StatistiqueTests {
+@ExtendWith(MockitoExtension.class)
+class StatistiqueTests {
 
-    @MockBean
-    StatistiqueImpl statistiqueImpl;
+    @Mock
+    private Echantillon echantillonMock;
 
+    @InjectMocks
+    private StatistiqueImpl statistiqueService;
+
+    @Test
+    void testCalculNombreVoitures() {
+        List<Voiture> faussesVoitures = List.of(
+            new Voiture("Tesla", 123),
+            new Voiture("BMW", 456)
+        );
+        
+        when(echantillonMock.getVoitures()).thenReturn(faussesVoitures);
+
+        int nb = statistiqueService.getNombreVoitures();
+
+        assertEquals(2, nb);
+        verify(echantillonMock, times(1)).getVoitures();
+    }
 }
